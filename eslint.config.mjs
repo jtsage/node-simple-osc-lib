a9647c746817cb8727e6e7719581e7ea792a768f
J.T. Sage <jtsage+datebox@gmail.com>
2026-08-11 11:39:50 -0400

    Fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,144 +1,153 @@
-import js from '@eslint/js'
-// import babelParser from '@babel/eslint-parser'
+// @ts-check
+
+import { defineConfig } from 'eslint/config'
 import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 import globals from 'globals'
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint'
 
-export default [
-	js.configs.recommended,
-	{
-		files : ['**/*.js'],
-		languageOptions : {
-			ecmaVersion : 2022,
-			
-			globals     : {
-				...globals.browser,
-				...globals.node,
-				...globals.es2021,
-				...globals.jest,
+export default defineConfig({
+	files : ['**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'],
+
+	extends : [js.configs.recommended, tseslint.configs.recommended],
+
+	languageOptions : {
+		ecmaVersion : 2022,
+		
+		globals     : {
+			...globals.browser,
+			...globals.node,
+			...globals.es2021,
+			...globals.jest,
+		},
+		parserOptions : {
+			requireConfigFile : false,
+		},
+		sourceType  : 'module',
+	},
+	plugins : {
+		unicorn : eslintPluginUnicorn,
+		'@stylistic' : stylistic,
+	},
+	rules : {
+
+		'@stylistic/comma-dangle' : [
+			'error',
+			{
+				'arrays'    : 'only-multiline',
+				'exports'   : 'never',
+				'functions' : 'never',
+				'imports'   : 'never',
+				'objects'   : 'always-multiline',
+			}
+		],
+		'@stylistic/indent' : [
+			'error',
+			'tab',
+			{ 'SwitchCase' : 1 },
+		],
+		'@stylistic/key-spacing' : [
+			'error',
+			{
+				'afterColon'  : true,
+				'beforeColon' : true,
+				'mode'        : 'minimum',
 			},
-			// parser      : babelParser,
-			parserOptions : {
-				requireConfigFile : false,
+		],
+		
+		'@stylistic/no-trailing-spaces' : [
+			'error',
+			{
+				'ignoreComments' : true,
+				'skipBlankLines' : true,
 			},
-			sourceType  : 'module',
-		},
-		plugins : {
-			unicorn : eslintPluginUnicorn,
-		},
-		rules : {
+		],
+		'no-unused-vars' : [
+			'error',
+			{
+				'args'              : 'all',
+				'argsIgnorePattern' : '^_',
+				'varsIgnorePattern' : '^_|^client',
+			},
+		],
+		'sort-keys' : [
+			'warn',
+			'asc',
+			{
+				'allowLineSeparatedGroups' : true,
+				'caseSensitive'            : false,
+				'minKeys'                  : 4,
+				'natural'                  : true,
+			}
+		],
 
+		'complexity'                      : ['warn', 30],
+		'default-case'                    : 'error',
+		'dot-notation'                    : 'error',
+		'eqeqeq'                          : 'error',
+		'no-await-in-loop'                : 'error',
+		'no-console'                      : 'warn',
+		'no-duplicate-imports'            : 'error',
+		'no-else-return'                  : 'error',
+		'no-global-assign'                : 'error',
+		'no-implicit-coercion'            : 'error',
+		'no-implicit-globals'             : 'error',
+		'no-lonely-if'                    : 'error',
+		'no-multi-str'                    : 'error',
+		'no-param-reassign'               : 'error',
+		'no-promise-executor-return'      : 'error',
+		'no-sequences'                    : 'error',
+		'no-shadow'                       : ['error', { 'builtinGlobals' : true }],
+		'no-template-curly-in-string'     : 'error',
+		'no-throw-literal'                : 'error',
+		'no-unneeded-ternary'             : 'error',
+		'no-unreachable-loop'             : 'error',
+		'no-unused-expressions'           : 'error',
+		'no-unused-private-class-members' : 'error',
+		'no-useless-backreference'        : 'error',
+		'no-useless-concat'               : 'error',
+		'no-var'                          : 'error',
+		'prefer-arrow-callback'           : 'error',
+		'prefer-const'                    : 'error',
+		'prefer-template'                 : 'error',
+		'require-atomic-updates'          : 'error',
+		
+		'@stylistic/array-bracket-spacing' : ['error', 'never'],
+		'@stylistic/arrow-parens'          : 'error',
+		'@stylistic/comma-spacing'         : 'error',
+		'@stylistic/function-call-spacing' : 'error',
+		'@stylistic/keyword-spacing'       : 'error',
+		'@stylistic/quotes'                : ['error', 'single'],
+		'@stylistic/semi'                  : ['error', 'never'],
 
-			'no-shadow'    : [ 'error', { 'builtinGlobals': true }],
-			'comma-dangle' : [
-				'error',
-				{
-					'arrays'    : 'only-multiline',
-					'exports'   : 'never',
-					'functions' : 'never',
-					'imports'   : 'never',
-					'objects'   : 'always-multiline',
-				}
-			],
-			'indent' : [
-				'error',
-				'tab',
-				{
-					'SwitchCase' : 1,
-				},
-			],
-			'key-spacing' : [
-				'error',
-				{
-					'afterColon'  : true,
-					'beforeColon' : true,
-					'mode'        : 'minimum',
-				},
-			],
-			'no-trailing-spaces' : [
-				'error',
-				{
-					'ignoreComments' : true,
-					'skipBlankLines' : true,
-				},
-			],
-			'no-unused-vars' : [
-				'error',
-				{
-					'args' : 'all',
-					'argsIgnorePattern' : '^_',
-					'varsIgnorePattern' : '^_|^client',
-				},
-			],
-	
-			'array-bracket-spacing'           : ['error', 'never'],
-			'arrow-parens'                    : 'error',
-			'comma-spacing'                   : 'error',
-			'default-case'                    : 'error',
-			'dot-notation'                    : 'error',
-			'eqeqeq'                          : 'error',
-			'func-call-spacing'               : 'error',
-			'keyword-spacing'                 : 'error',
-			'no-await-in-loop'                : 'error',
-			'no-console'                      : 'warn',
-			'no-duplicate-imports'            : 'error',
-			'no-else-return'                  : 'error',
-			'no-global-assign'                : 'error',
-			'no-implicit-coercion'            : 'error',
-			'no-implicit-globals'             : 'error',
-			'no-lonely-if'                    : 'error',
-			'no-multi-str'                    : 'error',
-			'no-param-reassign'               : 'error',
-			'no-promise-executor-return'      : 'error',
-			'no-return-await'                 : 'error',
-			'no-sequences'                    : 'error',
-			'no-template-curly-in-string'     : 'error',
-			'no-throw-literal'                : 'error',
-			'no-unneeded-ternary'             : 'error',
-			'no-unreachable-loop'             : 'error',
-			'no-unused-expressions'           : 'error',
-			'no-unused-private-class-members' : 'error',
-			'no-useless-backreference'        : 'error',
-			'no-useless-concat'               : 'error',
-			'no-var'                          : 'error',
-			'prefer-arrow-callback'           : 'error',
-			'prefer-const'                    : 'error',
-			'prefer-template'                 : 'error',
-			'quotes'                          : ['error', 'single'],
-			'require-atomic-updates'          : 'error',
-			'semi'                            : ['error', 'never'],
-	
-			'complexity'                      : ['warn', 30],
-			'sort-keys'                       : ['warn', 'asc', {'allowLineSeparatedGroups' : true, 'caseSensitive' : false, 'minKeys' : 4, 'natural' : true}],
-	
-			'unicorn/better-regex'                     : 'error',
-			'unicorn/catch-error-name'                 : ['error', { 'name' : 'err' }],
-			'unicorn/consistent-destructuring'         : 'error',
-			'unicorn/consistent-function-scoping'      : 'error',
-			'unicorn/empty-brace-spaces'               : 'error',
-			'unicorn/error-message'                    : 'error',
-			'unicorn/escape-case'                      : 'error',
-			'unicorn/explicit-length-check'            : ['error', { 'non-zero' : 'not-equal' }],
-			'unicorn/new-for-builtins'                 : 'error',
-			'unicorn/no-abusive-eslint-disable'        : 'error',
-			'unicorn/no-array-callback-reference'      : 'error',
-			'unicorn/no-array-method-this-argument'    : 'error',
-			'unicorn/no-array-push-push'               : 'error',
-			'unicorn/no-for-loop'                      : 'error',
-			'unicorn/no-lonely-if'                     : 'error',
-			'unicorn/no-this-assignment'               : 'error',
-			'unicorn/no-unnecessary-await'             : 'error',
-			'unicorn/no-unused-properties'             : 'error',
-			'unicorn/no-useless-length-check'          : 'error',
-			'unicorn/no-useless-spread'                : 'error',
-			'unicorn/no-useless-switch-case'           : 'error',
-			'unicorn/prefer-array-some'                : 'error',
-			'unicorn/prefer-json-parse-buffer'         : 'error',
-			'unicorn/prefer-native-coercion-functions' : 'error',
-			'unicorn/prefer-node-protocol'             : 'error',
-			'unicorn/prefer-set-has'                   : 'error',
-			'unicorn/prefer-spread'                    : 'error',
-			'unicorn/require-array-join-separator'     : 'error',
-			'unicorn/throw-new-error'                  : 'error',
-		},
-	}
-]
+		'unicorn/catch-error-name'                 : ['error', { 'name' : 'err' }],
+		'unicorn/consistent-destructuring'         : 'error',
+		'unicorn/consistent-function-scoping'      : 'error',
+		'unicorn/consistent-json-file-read'        : 'error',
+		'unicorn/empty-brace-spaces'               : 'error',
+		'unicorn/error-message'                    : 'error',
+		'unicorn/escape-case'                      : 'error',
+		'unicorn/explicit-length-check'            : ['error', { 'non-zero' : 'not-equal' }],
+		'unicorn/new-for-builtins'                 : 'error',
+		'unicorn/no-abusive-eslint-disable'        : 'error',
+		'unicorn/no-array-callback-reference'      : 'error',
+		'unicorn/no-array-method-this-argument'    : 'error',
+		'unicorn/no-for-loop'                      : 'error',
+		'unicorn/no-lonely-if'                     : 'error',
+		'unicorn/no-this-assignment'               : 'error',
+		'unicorn/no-unnecessary-await'             : 'error',
+		'unicorn/no-unused-properties'             : 'error',
+		'unicorn/no-useless-length-check'          : 'error',
+		'unicorn/no-useless-spread'                : 'error',
+		'unicorn/no-useless-switch-case'           : 'error',
+		'unicorn/prefer-array-some'                : 'error',
+		'unicorn/prefer-native-coercion-functions' : 'error',
+		'unicorn/prefer-node-protocol'             : 'error',
+		'unicorn/prefer-set-has'                   : 'error',
+		'unicorn/prefer-single-call'               : 'error',
+		'unicorn/prefer-spread'                    : 'error',
+		'unicorn/require-array-join-separator'     : 'error',
+		'unicorn/throw-new-error'                  : 'error',
+	},
+})
